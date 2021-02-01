5d0938f24c485b6f18bbeaf6443310e5a5f93361
fogset <zhangth1990@hotmail.com>
2021-01-31 21:48:15 -0800

    setting up signUpPage

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
-const app = express();
 const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
