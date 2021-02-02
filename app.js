1dd4313798a9ee7ec402fe990a62897176394169
fogset <zhangth1990@hotmail.com>
2021-02-01 23:58:40 -0800

    add mailchimap

--- app.js
+++ app.js
@@ -13,10 +13,21 @@ app.post("/", function(req, res){
   var firstName = req.body.fName;
   var lastName = req.body.lName;
   var email= req.body.email;
-  console.log(firstName,lastName,email);
+  var data ={
+    members:[
+      {
+        email_address: email,
+        status: "subscribed"
+      }
+    ]
+  }
 });
 
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 })
+
+//API Key
+//7ab3fd7a5144327dc7410630346661f9-us7
+//Audience id 66c53ff11c
