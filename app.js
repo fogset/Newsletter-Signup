a2f0e7c0f8ad28ffb755246ceaf7784eb9f0b3d2
fogset <zhangth1990@hotmail.com>
2021-02-02 23:59:32 -0800

    add a try again for user to go back previous page and try again

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ app.post("/", function(req, res){
   request.write(jsonData);
   request.end();
 });
-
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
