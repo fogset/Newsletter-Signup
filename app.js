ad91d02a01cd9626c8695683e3c013faed3c9097
fogset <zhangth1990@hotmail.com>
2021-02-02 23:16:05 -0800

    get people to sign up with email

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
+const conFig = require('./config');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -10,20 +11,34 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html")
 })
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email= req.body.email;
-  var data ={
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email= req.body.email;
+  const data ={
     members:[
       {
         email_address: email,
         status: "subscribed",
         merge_field:{
-
+          FNAME: firstName,
+          LNAME: lastName
         }
       }
     ]
-  }
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us7.api.mailchimp.com/3.0/lists/" + conFig.audienceKey;
+  const options ={
+    method: "POST",
+    auth: "tian:" + conFig.apiKey
+  };
+  const request = https.request(url,options,function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
 });
 
 
